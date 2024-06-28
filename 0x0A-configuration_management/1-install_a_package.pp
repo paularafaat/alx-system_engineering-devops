b58ffa8b6e93648e08e78495bb6812ed3a10b6fe
paula raafat <paularaafat27@.com>
2024-06-29 02:57:57 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
 #installing flsak
 package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3,
-    }
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
 package { 'Werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3,
-    }
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+

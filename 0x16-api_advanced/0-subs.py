f94c62367d0f7c9f3774c6943963940ccb13b72d
paula raafat <paularaafat27@.com>
2024-08-13 10:13:34 +0300

    number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function to return the num of sub"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "MyRedditScript/1.0 (by u/paula)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0

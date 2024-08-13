8efe2ea8cacde2035b787c17fb840de0558f339f
paula raafat <paularaafat27@.com>
2024-08-13 12:46:20 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""subs module"""
+"""
+    uses reddit api to print num of sub
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to return the num of sub"""
+    """
+        get the num of sub
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "MyRedditScript/1.0 (by u/paula)"
-    }
+    headers = {"User-Agent": "MyRedditScript/1.0 (by u/paula)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return 0
     res = response.json().get("data")
     return res.get("subscribers")

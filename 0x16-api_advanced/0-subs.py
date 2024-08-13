2e3cf63188bdc49de4183cdab9d506b7931bdf7f
paula raafat <paularaafat27@.com>
2024-08-13 12:20:08 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,5 +12,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    data = response.json()
-    return data["data"]["subscribers"]
+    res = response.json().get("data")
+    return res.get("subscribers")
